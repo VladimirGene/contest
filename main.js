41f48e3ec4bd37eaf4030b5392aba799f49e21ad
VladimirGene <149928759+VladimirGene@users.noreply.github.com>
2023-12-30 14:53:58 +0300

    add

---
+++ main.js
@@ -0,0 +1,21 @@
+function showTreasure() {
+	var treasure = document.getElementById("treasure");
+	treasure.classList.remove("hidden");
+	treasure.classList.add("visible");
+}
+
+function checkAnswer() {
+	var userAnswer = document.getElementById("answer").value.toLowerCase();
+	var result = document.getElementById("result");
+
+	if (userAnswer === "opasity: 1;" || userAnswer === "visibility: visible;") {
+		result.innerHTML = "Правильно! Вы нашли сокровище.";
+		result.classList.remove("incorrect");
+		result.classList.add("correct");
+		showTreasure();
+	} else {
+		result.innerHTML = "Неправильно. Используй свойства CSS. Попробуйте еще раз. Возможно ты пропусти пробел!";
+		result.classList.remove("correct");
+		result.classList.add("incorrect");
+	}
+}
